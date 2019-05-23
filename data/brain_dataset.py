d114970cdeaba796e98f36b777853cbdf64168b9
az4918 <az4918@ic.ac.uk>
2019-05-23 01:27:32 +0100

    adding dependency

--- data/brain_dataset.py
+++ data/brain_dataset.py
@@ -13,7 +13,7 @@ You need to implement the following functions:
 """
 import os.path
 from data.base_dataset import BaseDataset, get_transform
-from data.image_folder import make_dataset
+from data.image_folder import make_dataset_brain
 from PIL import Image
 import random
 import SimpleITK as sitk
