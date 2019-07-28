18cccca2a8deeb9d869700b8da1cccad7c0edaad
az4918 <az4918@ic.ac.uk>
2019-07-28 13:37:10 +0100

    printing netD type

--- train_time.py
+++ train_time.py
@@ -44,6 +44,8 @@ if __name__ == '__main__':
     visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots
     total_iters = 0                # the total number of training iterations
 
+    print("Using netD:", opt.netD)
+
     for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()    # timer for data loading per iteration
