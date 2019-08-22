Fixed rotation bug where images where rotated differently

data/base_dataset.py
@@ -74,9 +74,16 @@ def get_params(opt, size):
     y = random.randint(0, np.maximum(0, new_h - opt.crop_size))
 
     flip = random.random() > 0.5
-    rotate = random.random() > 0.5
 
-    return {'crop_pos': (x, y), 'flip': flip, 'rotate': rotate}
+    rotation = None
+
+    # If rotation is enabled, set it up and pass on the rotation object
+    if opt.rotate:
+        rotate = random.random() > 0.5
+        if rotate:
+            rotation = transforms.RandomRotation(opt.rotate)
+
+    return {'crop_pos': (x, y), 'flip': flip, 'rotation': rotation}
 
 
 def get_transform(opt, params=None, grayscale=False, method=Image.BICUBIC, convert=True):
@@ -95,12 +102,9 @@ def get_transform(opt, params=None, grayscale=False, method=Image.BICUBIC, conve
         else:
             transform_list.append(transforms.Lambda(lambda img: __crop(img, params['crop_pos'], opt.crop_size)))
 
-    if opt.rotate:
-        if params is None:
-            transform_list.append(transforms.RandomRotation(opt.rotate))
-        elif params['rotate']:
-            rot = transforms.RandomRotation(opt.rotate)
-            transform_list.append(transforms.Lambda(lambda img: rot(img)))
+    # If rotation is enabled above, use the random rotation object to apply to our images
+    if params['rotation']:
+        transform_list.append(transforms.Lambda(lambda img: params['rotation'](img)))
 
     if opt.preprocess == 'none':
         transform_list.append(transforms.Lambda(lambda img: __make_power_2(img, base=4, method=method)))