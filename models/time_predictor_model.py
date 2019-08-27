c0a5a2c6a99a0703e9084a59d35c3ef82920ba48
az4918 <az4918@ic.ac.uk>
2019-08-27 14:44:10 +0100

    Changed default netD to time_input from basic

--- models/time_predictor_model.py
+++ models/time_predictor_model.py
@@ -28,7 +28,7 @@ class TimePredictorModel(BaseModel):
         By default, we use UNet with batchnorm, and aligned datasets.
         """
         # changing the default values to match the pix2pix paper (https://phillipi.github.io/pix2pix/)
-        parser.set_defaults(norm='batch', dataset_mode='aligned')
+        parser.set_defaults(norm='batch', dataset_mode='aligned', netD='time_input')
         if is_train:
             parser.set_defaults(pool_size=0, gan_mode='vanilla')
             parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
